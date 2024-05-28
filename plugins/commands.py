ae0407a7daeaee29ddb4793eec3bc64ed6abff04
Kᴀᴍᴀʟᴀᴋᴀɴɴᴀɴ <97676159+UNKNOWN8884@users.noreply.github.com>
2024-05-28 15:17:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@GreyMattersYT') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ՏႮᏴՏᏟᎡᏆᏴᎬ🧡', url='https://youtube.com/@GreyMattersYT') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
